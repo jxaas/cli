41a119adddb6ec38e2486ab3e2e6e31528b83d8e
Justin SB <justin@fathomdb.com>
2014-05-04 23:55:38 -0700

    Pass port if provided for connecting to MySQL

--- jxaas/instances.py
+++ jxaas/instances.py
@@ -101,6 +101,8 @@ class ConnectInstance(cliff.command.Command):
           command = command + [ '--host=' + host ]
           command = command + [ '--password=' + properties['password'] ]
           command = command + [ '--database=' + properties['database'] ]
+          if properties['port']:
+            command = command + [ '--port=' + properties['port'] ]
 
         if relation == 'mongodb':
           if not host:
