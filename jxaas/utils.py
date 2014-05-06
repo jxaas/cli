faf95c7936d620b122b936216476843e80963d2d
Justin SB <justin@fathomdb.com>
2014-05-06 08:25:10 -0700

    Default to tenant1/user1/secret1

--- jxaas/utils.py
+++ jxaas/utils.py
@@ -6,9 +6,9 @@ from cliff.command import Command
 import jujuxaas.client
 
 def get_jxaas_client(command):
-  tenant = 'abcdef'
-  username = '123'
-  password= '123'
+  tenant = 'tenant1'
+  username = 'user1'
+  password= 'secret1'
 
   url = os.getenv('JXAAS_URL', "http://127.0.0.1:8080/xaas")
   client = jujuxaas.client.Client(url=url, tenant=tenant, username=username, password=password)
