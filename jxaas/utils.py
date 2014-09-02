78fdc29639547cd1922de34d1760d64438b955cc
Justin SB <justin@fathomdb.com>
2014-09-01 19:42:04 -0700

    Current fake-username & password == tenant

--- jxaas/utils.py
+++ jxaas/utils.py
@@ -7,8 +7,8 @@ import jujuxaas.client
 
 def get_jxaas_client(command):
   tenant = 'tenant1'
-  username = 'user1'
-  password= 'secret1'
+  username = 'tenant1'
+  password= 'tenant1'
 
   url = os.getenv('JXAAS_URL', "http://127.0.0.1:8080/xaas")
   client = jujuxaas.client.Client(url=url, tenant=tenant, username=username, password=password)
