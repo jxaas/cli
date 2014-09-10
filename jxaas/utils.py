40fed110ea725acd6806c1abf999861ac5ee399e
Justin SB <justin@fathomdb.com>
2014-09-09 21:43:25 -0700

    Initial work on OpenStack auth

--- jxaas/utils.py
+++ jxaas/utils.py
@@ -4,12 +4,19 @@ import os
 from cliff.command import Command
 
 import jujuxaas.client
+import jujuxaas.auth.direct
+import jujuxaas.auth.openstack
 
 def get_jxaas_client(command):
-  tenant = 'tenant1'
-  username = 'tenant1'
-  password= 'tenant1'
+  tenant = 'admin'
+  username = 'admin'
+  password= 'secret'
 
-  url = os.getenv('JXAAS_URL', "http://127.0.0.1:8080/xaas")
-  client = jujuxaas.client.Client(url=url, tenant=tenant, username=username, password=password)
+  #url = os.getenv('JXAAS_URL', "http://127.0.0.1:8080/xaas")
+  #auth = jujuxaas.auth.direct.AuthDirect(url=url, tenant=tenant, username=username, password=password)
+
+  url = os.getenv('JXAAS_URL', "http://127.0.0.1:5000")
+  auth = jujuxaas.auth.openstack.AuthOpenstack(url=url, username=username, password=password, tenant=tenant)
+
+  client = jujuxaas.client.Client(auth)
   return client
