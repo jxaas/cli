baf69a26edbb67bb6da9486a892bd9274e089345
Justin SB <justin@fathomdb.com>
2014-10-23 08:53:14 -0700

    Get username/password/auth-method from env vars

--- jxaas/utils.py
+++ jxaas/utils.py
@@ -8,15 +8,19 @@ import jujuxaas.auth.direct
 import jujuxaas.auth.openstack
 
 def get_jxaas_client(command):
-  tenant = 'admin'
-  username = 'admin'
-  password= 'secret'
+  tenant = os.getenv('JXAAS_TENANT', 'admin')
+  username = os.getenv('JXAAS_USER', 'admin')
+  password= os.getenv('JXAAS_SECRET', 'secret')
 
-  #url = os.getenv('JXAAS_URL', "http://127.0.0.1:8080/xaas")
-  #auth = jujuxaas.auth.direct.AuthDirect(url=url, tenant=tenant, username=username, password=password)
-
-  url = os.getenv('JXAAS_URL', "http://127.0.0.1:5000")
-  auth = jujuxaas.auth.openstack.AuthOpenstack(url=url, tenant=tenant, username=username, password=password)
+  auth = os.getenv('JXAAS_AUTH', 'direct')
+  if auth == 'direct':
+    url = os.getenv('JXAAS_URL', "http://127.0.0.1:8080/xaas")
+    auth = jujuxaas.auth.direct.AuthDirect(url=url, tenant=tenant, username=username, password=password)
+  elif auth == 'openstack':
+    url = os.getenv('JXAAS_URL', "http://127.0.0.1:5000")
+    auth = jujuxaas.auth.openstack.AuthOpenstack(url=url, tenant=tenant, username=username, password=password)
+  else:
+    raise Exception("Unknown authentication method specified: %s" % auth)
 
   client = jujuxaas.client.Client(auth)
   return client
